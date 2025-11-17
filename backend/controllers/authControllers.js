b7d15e883f3591a608843e8472bde6e351cc20a1
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-11-17 23:42:48 +0530

    make login

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -13,3 +13,20 @@ exports.signUp=async(req,res)=>{
         res.status(500).json({message:err.message});
     }
 }
+
+exports.login=async(req,res)=>{
+
+    const{email,password}=req.body;
+    try{
+        const user=await User.findOne({email});
+        if(!user) return res.status(400).json({message:"user not found"});
+
+        const ismatched=await bcrypt.compare(password,user.password);
+        if(!ismatched) return res.status(400).json({message:"Password not match"});
+
+        const token=jwt.sign({userId:user._id},process.env.JWT_SECERT,{expiresIn:'1h'});
+        res.json({token,user});
+    }catch(err){
+        res.status(500).json({message:err.message});
+    }
+}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express=require('express');
-const{signUp}=require('../controllers/authControllers');
+const{signUp,login}=require('../controllers/authControllers');
 const router=express.Router();
 
 router.post('/signup',signUp);
+router.post('/login',login);
 
 module.exports=router;
