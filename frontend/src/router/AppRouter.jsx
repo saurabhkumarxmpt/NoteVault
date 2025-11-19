577ccf3497b1716c09204bd715c014c4fd2cef03
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-11-19 22:12:48 +0530

    make some changes of the frontend side

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,15 +1,16 @@
 import {Routes, Route } from "react-router-dom";
-
 import MainLayout from "../layout/MainLayout";
 import HomePage from "../pages/HomePage";
 
 const AppRouter=()=>{
     return(
+        <>
         <Routes>
-            <Route element={<MainLayout/>}>
-                <Route path="/" element={<HomePage/>}/>
+            <Route path="/" element={<MainLayout/>}>
+                <Route index element={<HomePage/>}/>
             </Route>
         </Routes>
+        </>
     )
 }
 
